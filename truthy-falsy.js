be8fdb7fc10c37f7b8dbd80f4a6a72a6693e4526
AdrianoKishi <adrianokishigit config --global user.email adrianokishi@hotmail.com>
2022-08-29 23:42:48 -0300

    Exemplos da aula 01

---
+++ truthy-falsy.js
@@ -0,0 +1,23 @@
+// boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy or falsy
+
+// 0 -> false
+// 1 -> true
+
+// console.log(0 == false);
+// console.log("" == false);
+// console.log(1 == true);
+
+// null
+// undefined
+
+let minhaVar;           // tipo number
+let varNull = null;     // tipo objeto, curioso.
+
+let numero = 3;      
+let texto = "Alura"; 
+console.log(typeof minhaVar);
+console.log(typeof varNull);
