bf30e5d889845b31751d7cf4becaef6a2d790fe1
AdrianoKishi <adrianokishigit config --global user.email adrianokishi@hotmail.com>
2022-08-29 22:19:32 -0300

    Exemplos da aula 01

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+// boolean
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+// 2 e 3 sinais de iguais nos retornam true or false;
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
---
+++ type-number.js
@@ -0,0 +1,22 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5; //Não precisa digitar o 0 na frente, não é necessario, o js ja entende.
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NaN = Not a Number
+
+const alura = "Alura";
+console.log(alura*primeiroNumero);
